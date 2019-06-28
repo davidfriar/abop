26559d27063ea30fa78ac58650411787cc50d028
David Friar <david.friar@cognifide.com>
2019-06-28 23:43:27 +0100

    benchmark

--- abop_lib/benches/bench.rs
+++ abop_lib/benches/bench.rs
@@ -6,19 +6,54 @@ use criterion::{BatchSize, Bencher, Criterion};
 
 use abop_lib::parser;
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn lsys_simple(c: &mut Criterion) {
+    lsys_benchmark(
+        c,
+        "lsys-simple",
+        r#"
+            F
+            F=FF
+        "#,
+        10,
+    );
+}
+fn lsys_koch(c: &mut Criterion) {
+    lsys_benchmark(
+        c,
+        "lsys-koch",
+        r#"
+            F-F-F-F
+            F=F-F+F+FF-F-F+F
+        "#,
+        5,
+    );
+}
+
+fn lsys_params(c: &mut Criterion) {
+    lsys_benchmark(
+        c,
+        "lsys-params",
+        r#"
+            F(0.01)
+            F(x)=F(x*2)+(45)F(x*2)-(60)
+        "#,
+        10,
+    );
+}
+
+fn lsys_benchmark(c: &mut Criterion, title: &str, data: &'static str, iters: usize) {
     c.bench_function_over_inputs(
-        "generate",
-        |b: &mut Bencher, n: &usize| {
+        title,
+        move |b: &mut Bencher, n: &usize| {
             b.iter_batched(
-                || parser::parse_lsys("F\nF=FF"),
+                move || parser::parse_lsys(data),
                 |mut lsys| lsys.nth(black_box(*n)),
                 BatchSize::PerIteration,
             )
         },
-        0..20,
+        0..iters,
     );
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, lsys_simple, lsys_koch, lsys_params);
 criterion_main!(benches);
